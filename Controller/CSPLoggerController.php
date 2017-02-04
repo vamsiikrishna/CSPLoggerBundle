360102627a8b29f6180c9c6ab4fa6bfcefba3d3c
Vamsi Krishna B <vamsi@fastmail.com>
2017-02-04 19:06:27 +0530

    Simplification

--- Controller/CSPLoggerController.php
+++ Controller/CSPLoggerController.php
@@ -15,22 +15,15 @@ class CSPLoggerController extends Controller
         $csp_report = new CSPReport();
         $form = $this->createForm(CSPReportType::class, $csp_report);
         if ($request->isMethod('POST')) {
-            $data = json_decode($request->getContent(), true);
-            $form->submit($data);
-
+            $data = array_values(json_decode($request->getContent(), true));
+            $form->submit($data[0]);
             if ($form->isSubmitted() && $form->isValid()) {
-                $csp_report->setDocumentUri($data['csp-report']['document-uri']);
-                $csp_report->setReferrer($data['csp-report']['referrer']);
-                $csp_report->setBlockedUri($data['csp-report']['blocked-uri']);
-                $csp_report->setViolatedDirective($data['csp-report']['violated-directive']);
-                $csp_report->setOriginalPolicy($data['csp-report']['original-policy']);
                 $csp_report->setDateReceived(new \DateTime());
                 $csp_report->setSenderIp($request->getClientIp());
                 $csp_report->setUserAgent($request->headers->get('User-Agent'));
                 $em = $this->get('doctrine.orm.default_entity_manager');
                 $em->persist($csp_report);
                 $em->flush($csp_report);
-
                 return new Response($csp_report->getId());
             } else {
                 return new Response('OOPS');
--- Form/Type/CSPReportType.php
+++ Form/Type/CSPReportType.php
@@ -3,6 +3,7 @@
 namespace Sockam\CSPLoggerBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,11 +15,21 @@ class CSPReportType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('documentUri')
-            ->add('referrer')
-            ->add('blockedUri')
-            ->add('violatedDirective')
-            ->add('originalPolicy');
+            ->add('document-uri',TextType::class, [
+                'property_path' => 'documentUri'
+            ])
+            ->add('referrer', TextType::class, [
+                'property_path' => 'referrer'
+            ])
+            ->add('blocked-uri', TextType::class, [
+                'property_path' => 'blockedUri'
+            ])
+            ->add('violated-directive', TextType::class, [
+                'property_path' => 'violatedDirective'
+            ])
+            ->add('original-policy', TextType::class, [
+                'property_path' => 'originalPolicy'
+            ]);
     }
 
     /**
